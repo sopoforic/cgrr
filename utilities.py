b58cdbbb507dd559b9484a8375b476c214646e58
Tracy Poff <tracy.poff@gmail.com>
2014-04-20 20:06:19 -0400

    Now FileReader supports more general pre-processing of the data.
    FileReader now accepts two dictionaries (massage_in and massage_out) that
    map variable names to functions that take the raw value and output a processed
    value (or the reverse, respectively). This can be used for decoding bytes to
    strings and stripping them, turning ASCII integers into real numbers, or
    whatever else makes sense. Variables without keys in the dictionaries are used
    as-is.

--- utilities.py
+++ utilities.py
@@ -45,35 +45,68 @@ File = namedtuple("File", ["path", "size", "md5"])
 
 class FileReader(object):
     """Reads files into dictionaries."""
-    def __init__(self, format, byte_order="<", encoding="ascii"):
+    def __init__(self, format, massage_in=None, massage_out=None, byte_order="<"):
         """Create a reader for a specific file format.
 
         format should be a list of tuples in the form
 
-        (name, format_string)
+        (variable_name, format_string)
 
         where format_string is a struct-style description of the variable.
+
+        If present, massage_in and massage_out should be dictionaries mapping
+        (a subset of) the variable names from format to functions that massage
+        the raw value into a user-friendly format and from said user-friendly
+        format back into the raw data required by the file, respectively.
+        
+        An example input processing function might be:
+        
+            { 'player_name' : (lambda s: s.strip('\x00')) }
+        
+        which would strip null padding from a string.
+        
+        Any variables which are not in the dictionaries will be used as-is.
 
         """
         self.format = OrderedDict(format)
         self.byte_order = byte_order
-        self.encoding = encoding
         self.fmt = byte_order + "".join(self.format.values())
         self.struct = struct.Struct(self.fmt)
+        self.massage_in = massage_in
+        self.massage_out = massage_out
 
     def unpack(self, data):
         vals = self.struct.unpack(data)
         dictionary = dict(zip(
             filter(lambda x: x[:7] != 'padding', self.format.keys()), vals))
-        for k in dictionary:
-            if type(dictionary[k]) is bytes and k[:3] != "raw":
-                dictionary[k] = dictionary[k].decode(self.encoding).strip('\x00')
+        # If the data needs processing before being sent to the user, then the
+        # user should have passed a dictionary massage_in with entries like
+        #
+        #     { 'variable_name' : processing_function }
+        #
+        # which can be used to massage the data into the desired format. This
+        # should be paired with a massage_out dictionary (used by pack) to
+        # turn things back into the required format.
+        #
+        # An example input processing function might be:
+        #
+        #     { 'player_name' : (lambda s: s.strip('\x00')) }
+        #
+        # which would strip null padding from a string.
+        #
+        # Any variables which are not in the dictionary will be used as-is.
+        if self.massage_in:
+            for k in dictionary:
+                if k in self.massage_in:
+                    dictionary[k] = self.massage_in[k](dictionary[k])
         return dictionary
 
-    def pack(self, data):
-        for k in data:
-            if type(data[k]) is string:
-                data[k] = data[k].encode(self.encoding)
-        vals = [data[key] for key in
+    def pack(self, dictionary):
+        # See the comment on unpack.
+        if self.massage_out:
+            for k in dictionary:
+                if k in self.massage_out:
+                    dictionary[k] = self.massage_out[k](dictionary[k])
+        vals = [dictionary[key] for key in
                 filter(lambda x: x[:7] != 'padding', self.format.keys())]
         return self.struct.pack(vals)
