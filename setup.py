0295f60d51c338184b09b74af492d6593e0fc4c9
Tracy Poff <tracy.poff@gmail.com>
2015-05-31 18:53:03 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='cgrr',
+      version='1.0',
+      description='Classic Game Resource Reader support files',
+      author='Tracy Poff',
+      author_email='tracy.poff@gmail.com',
+      py_modules=['cgrr'],
+      )
