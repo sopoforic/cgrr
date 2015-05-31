fa8b8ccba23bacc2b23dd0e4ed76067c63b96a85
Tracy Poff <tracy.poff@gmail.com>
2015-05-31 18:53:46 -0400

    Update cgrr.py docstring slightly.

--- cgrr.py
+++ cgrr.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with CGRR.  If not, see <http://www.gnu.org/licenses/>.
-"""Utilities for cgrr."""
+"""Utilities for cgrr modules."""
 import os
 import hashlib
 import struct
